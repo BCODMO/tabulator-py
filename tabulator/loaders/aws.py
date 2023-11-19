d0c37a7bb0241b4fbf2260ae1dcdcef6378a63d6
Conrad S <conrad.schloer@gmail.com>
2023-11-19 01:43:09 +0100

    Add load in memory code

--- tabulator/loaders/aws.py
+++ tabulator/loaders/aws.py
@@ -4,9 +4,14 @@ from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+from multiprocessing import shared_memory
+import time
 import os
 import io
 import boto3
+import base64
+import zlib
+
 from six.moves.urllib.parse import urlparse
 from ..loader import Loader
 from .. import exceptions
@@ -16,48 +21,77 @@ from .. import config
 
 # Module API
 
+
+def _encode_string(s):
+    """Encode a string that is url/path not safe to a base64 string"""
+    compressed_bytes = zlib.compress(s.encode("utf-8"), 9)
+    encoded_bytes = base64.urlsafe_b64encode(compressed_bytes)
+    encoded_str = str(encoded_bytes, "utf-8")
+    return encoded_str
+
+
 class AWSLoader(Loader):
-    """Loader to load source from the AWS.
-    """
+    """Loader to load source from the AWS."""
 
     # Public
 
     remote = True
     options = [
-        's3_endpoint_url',
+        "s3_endpoint_url",
     ]
 
-    def __init__(self,
-                 bytes_sample_size=config.DEFAULT_BYTES_SAMPLE_SIZE,
-                 s3_endpoint_url=None):
+    def __init__(
+        self, bytes_sample_size=config.DEFAULT_BYTES_SAMPLE_SIZE, s3_endpoint_url=None
+    ):
         self.__bytes_sample_size = bytes_sample_size
         self.__s3_endpoint_url = (
-            s3_endpoint_url or
-            os.environ.get('S3_ENDPOINT_URL') or
-            config.S3_DEFAULT_ENDPOINT_URL)
-        self.__s3_client = boto3.client('s3', endpoint_url=self.__s3_endpoint_url)
+            s3_endpoint_url
+            or os.environ.get("S3_ENDPOINT_URL")
+            or config.S3_DEFAULT_ENDPOINT_URL
+        )
+        self.__s3_client = boto3.client("s3", endpoint_url=self.__s3_endpoint_url)
         self.__stats = None
 
     def attach_stats(self, stats):
         self.__stats = stats
 
-    def load(self, source, mode='t', encoding=None):
+    def load(self, source, mode="t", encoding=None):
 
         # Prepare bytes
         try:
-            parts = urlparse(source, allow_fragments=False)
-            response = self.__s3_client.get_object(Bucket=parts.netloc, Key=parts.path[1:])
-            # https://github.com/frictionlessdata/tabulator-py/issues/271
-            bytes = io.BufferedRandom(io.BytesIO())
-            bytes.write(response['Body'].read())
-            bytes.seek(0)
+            try:
+                shm_key = _encode_string(source)
+                existing_shm = shared_memory.SharedMemory(name=shm_key)
+                print("Using existing shared memory")
+
+                start = time.time()
+                bytes = io.BufferedRandom(io.BytesIO())
+                bytes.write(existing_shm.buf)
+                bytes.seek(0)
+                # existing_shm.close()
+                print(f"Took {time.time() - start} for shared memory")
+            except:
+                # print("Not using shared memory")
+                start = time.time()
+                parts = urlparse(source, allow_fragments=False)
+                response = self.__s3_client.get_object(
+                    Bucket=parts.netloc, Key=parts.path[1:]
+                )
+                # https://github.com/frictionlessdata/tabulator-py/issues/271
+                bytes = io.BufferedRandom(io.BytesIO())
+                contents = response["Body"].read()
+                bytes.write(contents)
+                bytes.seek(0)
+                print(f"Took {time.time() - start} to load in the file")
+                print(f"Passing on at {time.time()}")
+
             if self.__stats:
                 bytes = helpers.BytesStatsWrapper(bytes, self.__stats)
         except Exception as exception:
             raise exceptions.LoadingError(str(exception))
 
         # Return bytes
-        if mode == 'b':
+        if mode == "b":
             return bytes
 
         # Detect encoding
